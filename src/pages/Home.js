b10f7117ddb0b7e0722f32161cd5af1866210996
Winga7 <andywinga@hotmail.be>
2024-08-19 18:32:02 +0200

    chore: Supprimer le titre "Accueil" de la page d'accueil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,6 @@ import images from "../storage/homepageCarousel.json";
  */
 export const Home = (element) => {
   element.innerHTML = `
-    <h1 class="Accueil">Accueil</h1>
     <p class="Accueil">Bienvenue sur CroqueMinute <img src="/img/croqueminute.png" id="accueilimg" alt="" srcset="" /></p>
     <p class="Accueil">Vos plats préférés, prêts en un clin d'oeil !</p>
     <p id="suggestion" class="Accueil">Nos suggestion</p>
