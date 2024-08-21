27913a8ef8bd4294d1bda303ba247a8f90d57d47
Winga7 <andywinga@hotmail.be>
2024-08-21 02:05:46 +0200

    chore: Mettre à jour le lien de l'icône de la page d'accueil

--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -36,7 +36,7 @@ export const Nav = (element) => {
   );
 
   element.innerHTML = `
-    <nav class="navbar navbar-expand-lg bg-dark-subtle">
+    <nav class="navbar navbar-expand-lg bg-navbar">
       <div class="container-fluid">
         <a class="navbar-brand" href="/">${appName}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
